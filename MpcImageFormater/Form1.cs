3a0bd1153108d02fef78f505d2f7d0f91a5a2fa5
Philippe Cayouette <pcayouette@spoluck.ca>
2018-05-30 09:37:52 -0400

    lib removal

--- MpcImageFormater/Form1.cs
+++ MpcImageFormater/Form1.cs
@@ -11,7 +11,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-using MtgApiManager.Lib.Service;
 
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
